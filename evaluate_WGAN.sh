800fa2cb2bec5ff4dec2cab6cc119692269211ab
Noambsf <noambsf@gmail.com>
2024-11-11 19:02:13 +0100

    Add convert.py in the evaluate.sh script

--- evaluate_WGAN.sh
+++ evaluate_WGAN.sh
@@ -2,5 +2,5 @@ python generate_WGAN.py --checkpoints_folder checkpoints2_wgan \
                         --G_model_name G_wgan_pretrained150_e30.pth\
                         --D_model_name D_wgan_pretrained150_e30.pth\
                         --rejection_sampling True
-
+python convert.py # Converts the .ubyte file in a folder of .jpg images
 python evaluate.py
